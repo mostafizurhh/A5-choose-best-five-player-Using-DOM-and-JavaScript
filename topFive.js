ab744a7c400948f3a9f649cb3af9c7cbcf030566
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-22 04:49:38 +0100

    function added in js file to append players

--- topFive.js
+++ topFive.js
@@ -1,4 +1,15 @@
+function appendChildren(selectPlayer) {
+    const player = document.getElementById(selectPlayer);
+    const playerName = player.innerText;
+    const orderdList = document.getElementById('appendPlayers');
+    const newItem = document.createElement('li');
+    newItem.innerText = playerName;
+    orderdList.appendChild(newItem);
+    const button = document.getElementById('selectMessi');
+    button.disabled = true;
+    // console.log(OL.children.length);
+}
 
-const OL = document.getElementById('appendPlayers');
-console.log(OL);
-console.log(OL.children.length);
+document.getElementById('selectMessi').addEventListener('click', function () {
+    appendChildren('messi');
+})
